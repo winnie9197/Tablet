778ea6a52e3c9c2e25634e88eb7fc8e204c692ef
winniehcy <winniehcyeung@gmail.com>
2020-09-16 01:25:40 -0400

    readme photos

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -4,9 +4,9 @@ export default {
             _id: 1,
             description: "A quick one-liner describing the contact, by the contact… Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             relationship: "Acquainted",
-            how: "Personal description...",
-            where: "Personal description...",
-            personalNote: "No note yet",
+            how: "It's a secret...",
+            where: "Between you two...",
+            personalNote: "A sweet, gentle person. Would love to see her again!",
         }  
     ]
 }
